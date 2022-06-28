9d43a81b99ef3415c8413321cf9a91c6a438e6cd
Eunji Park <80046101+SavinPark@users.noreply.github.com>
2022-06-28 21:22:50 +0900

    Nutrition 로직 구현

--- src/pages/NutritionAnalysis.jsx
+++ src/pages/NutritionAnalysis.jsx
@@ -12,8 +12,15 @@ function NutritionAnalysis() {
     const [existData, setExistData] = useState(false)
 
     const seizeIngredient = () => {
-
-        return '2%20egg';
+        let ingres = '';
+        ingreKey.forEach((key, idx) => {
+            if (idx + 1 === ingreKey.length) {
+                ingres += $(`#quentity-${key}`).val() + $(`#unit-${key}`).val() + "%20" + $(`#ingredient-${key}`).val();
+            } else {
+                ingres += $(`#quentity-${key}`).val() + $(`#unit-${key}`).val() + "%20" + $(`#ingredient-${key}`).val() + "%20";
+            }
+        })
+        return ingres;
     }
 
     const analyze = () => {
@@ -22,14 +29,16 @@ function NutritionAnalysis() {
         const APP_KEY =  process.env.REACT_APP_EDAMAM_API_KEY_NUTRITION;
         axios.get(`https://api.edamam.com/api/nutrition-data?app_id=${APP_ID}&app_key=${APP_KEY}&nutrition-type=cooking&ingr=${INGREDIENTS}`)
         .then(response => {
-            setResult(response.data.calories);
-            console.log(result);
             setExistData(true);
-            // console.log(response)
+            setResult(response.data);
         })
         .catch(error => {
             console.error(error);
         })
+    }
+
+    const mathRound = (number) => {
+        return  Math.round(( number + Number.EPSILON) * 100) / 100
     }
 
     const addIngre = () => {
@@ -100,48 +109,68 @@ function NutritionAnalysis() {
                 { existData && (
                     <div className="result">
                         <dl className="calories">
-                            <dt><span>Amount Per Serving</span>Calories</dt>
-                            <dd>125822</dd>
+                            <dt>
+                                <p>Amount Per Serving</p>
+                                <p>Calories</p>
+                            </dt>
+                            <dd>{mathRound(result.calories)}</dd>
+                        </dl>
+                        <dl className="daily-value">
+                            <dd>% Daily Value *</dd>
                         </dl>
                         <dl className="fat">
                             <dt>Total Fat</dt>
-                            <dd>6703.6 g</dd>
-                            <dd>10313 %</dd>
+                            <dd>{mathRound(result.totalNutrients.FAT.quantity)} {result.totalNutrients.FAT.unit}</dd>
+                            <dd>{mathRound(result.totalDaily.FAT.quantity)} {result.totalDaily.FAT.unit}</dd>
                             <dt>Saturated Fat</dt>
-                            <dd>6703.6 g</dd>
-                            <dd>10313 %</dd>
+                            <dd>{mathRound(result.totalNutrients.FASAT.quantity)} {result.totalNutrients.FASAT.unit}</dd>
+                            <dd>{mathRound(result.totalDaily.FASAT.quantity)} {result.totalDaily.FASAT.unit}</dd>
                             <dt>Trans Fat</dt>
-                            <dd>-</dd>
+                            <dd>{mathRound(result.totalNutrients.FATRN.quantity)} {result.totalNutrients.FATRN.unit}</dd>
                         </dl>
                         <dl className="cholesterol">
                             <dt>Cholesterol</dt>
-                            <dd>20626.6 mg</dd>
-                            <dd>6876 %</dd>
+                            <dd>{mathRound(result.totalNutrients.CHOLE.quantity)} {result.totalNutrients.CHOLE.unit}</dd>
+                            <dd>{mathRound(result.totalDaily.CHOLE.quantity)} {result.totalDaily.CHOLE.unit}</dd>
                         </dl>
                         <dl className="sodium">
                             <dt>Sodium</dt>
-                            <dd>20626.6 mg</dd>
-                            <dd>6876 %</dd>
+                            <dd>{mathRound(result.totalNutrients.NA.quantity)} {result.totalNutrients.NA.unit}</dd>
+                            <dd>{mathRound(result.totalDaily.NA.quantity)} {result.totalDaily.NA.unit}</dd>
                         </dl>
                         <dl className="carbohydrate">
-                            <dt>Sodium</dt>
-                            <dd>20626.6 mg</dd>
-                            <dd>6876 %</dd>
+                            <dt>Total Carbohydrate</dt>
+                            <dd>{mathRound(result.totalNutrients.NA.quantity)} {result.totalNutrients.NA.unit}</dd>
+                            <dd>{mathRound(result.totalDaily.NA.quantity)} {result.totalDaily.NA.unit}</dd>
+                            <dt>Dietary Fiber</dt>
+                            <dd>{mathRound(result.totalNutrients.FIBTG.quantity)} {result.totalNutrients.FIBTG.unit}</dd>
+                            <dt>Total Sugars</dt>
+                            <dd>{mathRound(result.totalNutrients.SUGAR.quantity)} {result.totalNutrients.SUGAR.unit}</dd>
                         </dl>
                         <dl className="protein">
-                            <dt>Total Fat</dt>
-                            <dd>6703.6 g</dd>
-                            <dd>10313 %</dd>
+                            <dt>Protein</dt>
+                            <dd>{mathRound(result.totalNutrients.PROCNT.quantity)} {result.totalNutrients.PROCNT.unit}</dd>
+                            <dd>{mathRound(result.totalDaily.PROCNT.quantity)} {result.totalDaily.PROCNT.unit}</dd>
                         </dl>
                         <dl className="vitaminD">
                             <dt>Vitamin D</dt>
-                            <dd>6703.6 g</dd>
-                            <dd>10313 %</dd>
+                            <dd>{mathRound(result.totalNutrients.VITD.quantity)} {result.totalNutrients.VITD.unit}</dd>
+                            <dd>{mathRound(result.totalDaily.VITD.quantity)} {result.totalDaily.VITD.unit}</dd>
                         </dl>
                         <dl className="calcium">
                             <dt>Calcium</dt>
-                            <dd>6703.6 g</dd>
-                            <dd>10313 %</dd>
+                            <dd>{mathRound(result.totalNutrients.CA.quantity)} {result.totalNutrients.CA.unit}</dd>
+                            <dd>{mathRound(result.totalDaily.CA.quantity)} {result.totalDaily.CA.unit}</dd>
+                        </dl>
+                        <dl className="iron">
+                            <dt>Iron</dt>
+                            <dd>{mathRound(result.totalNutrients.FE.quantity)} {result.totalNutrients.FE.unit}</dd>
+                            <dd>{mathRound(result.totalDaily.FE.quantity)} {result.totalDaily.FE.unit}</dd>
+                        </dl>
+                        <dl className="potassium">
+                            <dt>Potassium</dt>
+                            <dd>{mathRound(result.totalNutrients.K.quantity)} {result.totalNutrients.K.unit}</dd>
+                            <dd>{mathRound(result.totalDaily.K.quantity)} {result.totalDaily.K.unit}</dd>
                         </dl>
                     </div>
                 )}
